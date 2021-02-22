1c884cce83bcbc6c2f622f2419269fea5d81b568
Sushek Tamrakar <sushek@pandamotions.com>
2021-02-22 17:56:10 +0545

    fb api solved

--- Cms/models.py
+++ Cms/models.py
@@ -9,7 +9,6 @@ class AboutUs(models.Model):
 
 	def __str__(self):
 
-<<<<<<< HEAD
 		return self.title 
 
 
@@ -27,43 +26,12 @@ class PrivacyAndPolicy(models.Model):
 
 	title = models.CharField(max_length=100)
 	body = RichTextField()
-=======
-
-		return self.title 
-
-
-class TermsAndConditions(models.Model):
-
-	title = models.CharField(max_length=100)
-	body = RichTextField()
-
-	def __str__(self):
-
-		return self.title 
-
-
-class PrivacyAndPolicy(models.Model):
-
-
-	title = models.CharField(max_length=100)
-	body = RichTextField()
 
 
 	def __str__(self):
-
-		return self.title
-
->>>>>>> 16b9d16ff5113de2d04fa151e02cb0d18fad2166
-
-
-
-<<<<<<< HEAD
 		return self.title 
 
 
-=======
-
->>>>>>> 16b9d16ff5113de2d04fa151e02cb0d18fad2166
 
 class Banner(models.Model):
 	
